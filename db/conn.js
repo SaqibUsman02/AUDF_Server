149ff2e831927490276c271a22ad6e2600182390
SaqibUsman02 <tech54qi@gmail.com>
2023-05-15 23:42:55 +0500

    q

--- db/conn.js
+++ db/conn.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 
 const DB = process.env.DATABASE;
 
-mongoose.connect(DB,{
+mongoose.connect('mongodb+srv://discussionforum:discussionforum@cluster0.chjaowf.mongodb.net/discussionforum?retryWrites=true&w=majority',{
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(() => {
