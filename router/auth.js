0ee334d462860c4b7f4eca1a1caaab3273942123
SaqibUsman02 <tech54qi@gmail.com>
2023-06-13 18:03:07 +0500

    asdw

--- router/auth.js
+++ router/auth.js
@@ -1233,7 +1233,7 @@ token = await user.generateAuthToken();
 
 // ---------------
 
-router.get("/users/:id/verify/:token", async (req, res) => {
+router.get("https://df-kappa.vercel.app/users/:id/verify/:token", async (req, res) => {
   try {
    
     const user = await User.findOne({ _id: req.params.id });
